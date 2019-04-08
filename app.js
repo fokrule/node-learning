03c03c9f59e237e78d9310b232f4c92bb5e65f64
fokrule <fokrule.hasan"sjinnovation.com>
2019-04-08 23:24:38 +0600

    Done reading and writing file functionality

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
-var stuff = require('./stuff');
+var fs =  require('fs'); // this is the way to require any node library. here fs is a node library
 
-console.log(stuff.counter(['fokrul','hasan','forhad']));
-console.log(stuff.adder(3,4));
-console.log(stuff.adder(stuff.pi,3));
+// those are synchronous code. So those will run first then other code. >> started
+var readMe = fs.readFileSync('about.txt', 'utf8');
+
+fs.writeFileSync('new-about2.txt', readMe);
+
+// those are synchronous code. So those will run first then other code. << end
+
+fs.readFile('about.txt', 'utf8', function(err, data) {
+	fs.writeFile('esync.txt', data);
+});
+console.log('this will run before above asynchronous code. Because asyc function never stop other code');
