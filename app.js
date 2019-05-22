8d4b3602361aa581cd224ba47315b82e7fb1e40b
fokrule <fokrule.hasan"sjinnovation.com>
2019-05-22 20:05:29 +0600

    Done rreadable data from file

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 var http = require('http'); // load http module
 
+// here we will use fs module or file systme module to read data
 
-var server = http.createServer(function(request, response) {
+var fs = require('fs');
+
+var myReadStream = fs.createReadStream(__dirname + '/about.txt', 'utf8'); // create read able stram and read about.tzt file
+
+myReadStream.on('data', function(chunk){
+	console.log('Chunk received');
+	console.log(chunk);
+});
+
+
+/*var server = http.createServer(function(request, response) {
 	console.log('request : ' + request.url);
 	response.writeHead(200, {'Content-Type' : 'text/plain'});
 	response.end('Hey Forhad');
 }); // create server
 
 server.listen(3000, '127.0.0.1');
-console.log('Listening');
+console.log('Listening');*/
