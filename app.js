89db6396be0ebe56904fd6d99c0a5503ddcac1c9
fokrule <fokrule.hasan"sjinnovation.com>
2019-05-22 22:41:43 +0600

    Done serving json data

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
-var http = require('http'); // load http module
-
-// here we will use fs module or file systme module to read data
+var http = require('http');
 
 var fs = require('fs');
 
-
-var server = http.createServer(function(request, response) {
-	console.log('request : ' + request.url);
-	response.writeHead(200, {'Content-Type' : 'text/html'});
-	var myReadStream = fs.createReadStream(__dirname + '/index.html', 'utf8'); // create read able stram and read about.tzt file
-	myReadStream.pipe(response);
-}); // create server
+var server = http.createServer(function(request, response){
+	console.log('Request was made : ' + request.url );
+	response.writeHead(200, {'Content-Type' : 'application/json'});
+	var myObject = {
+		name : 'Forhad',
+		age : 26,
+		profession : 'Software Engineer'
+	};
+	response.end(JSON.stringify(myObject));
+});
 
 server.listen(3000, '127.0.0.1');
-console.log('Listening');
+console.log('Listening to the server');
