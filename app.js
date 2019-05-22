6e34fc3073fa8b81229a8590f458f24d9e398958
fokrule <fokrule.hasan"sjinnovation.com>
2019-05-22 22:14:03 +0600

    Done PIPE

--- app.js
+++ app.js
@@ -4,19 +4,13 @@ var http = require('http'); // load http module
 
 var fs = require('fs');
 
-var myReadStream = fs.createReadStream(__dirname + '/about.txt', 'utf8'); // create read able stram and read about.tzt file
-var myWriteStream = fs.createWriteStream(__dirname + '/about-write.txt');
-myReadStream.on('data', function(chunk){
-	console.log('Chunk received');
-	myWriteStream.write(chunk); // the data got by chunk will store in about-write.txt
-});
 
-
-/*var server = http.createServer(function(request, response) {
+var server = http.createServer(function(request, response) {
 	console.log('request : ' + request.url);
 	response.writeHead(200, {'Content-Type' : 'text/plain'});
-	response.end('Hey Forhad');
+	var myReadStream = fs.createReadStream(__dirname + '/about.txt', 'utf8'); // create read able stram and read about.tzt file
+	myReadStream.pipe(response);
 }); // create server
 
 server.listen(3000, '127.0.0.1');
-console.log('Listening');*/
+console.log('Listening');
