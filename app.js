2b406027e46433a590303fadcd4b9f199bf9568e
Fokrul <fokrule.hasan@sjinnovation.com>
2019-05-23 16:20:59 +0600

    done basic routing

--- app.js
+++ app.js
@@ -4,13 +4,34 @@ var fs = require('fs');
 
 var server = http.createServer(function(request, response){
 	console.log('Request was made : ' + request.url );
-	response.writeHead(200, {'Content-Type' : 'application/json'});
-	var myObject = {
-		name : 'Forhad',
-		age : 26,
-		profession : 'Software Engineer'
-	};
-	response.end(JSON.stringify(myObject));
+	if (request.url === '/home' || request.url === '/'){
+		console.log('dd');
+		response.writeHead(200, {'Content-Type' : 'text/html'});
+		fs.createReadStream(__dirname + '/index.html').pipe(response);
+	}
+	else if (request.url === '/contact'){
+		console.log(response.url);
+		response.writeHead(200, {'Content-Type' : 'text/html'});
+		fs.createReadStream(__dirname + '/contact.html').pipe(response);
+	}
+	else if (request.url === '/api/user') {
+		var user = [
+			{
+				name : 'Forhad',
+				age : 26
+			},
+			{
+				name : 'Fokrul',
+				age : 25
+			}
+		];
+		response.writeHead(200, {'Content-Type' : 'application/json'});
+		response.end(JSON.stringify(user));		
+	}
+	else {
+		response.writeHead(400, {'Content-Type': 'text/html'});
+		fs.createReadStream(__dirname + '/404.html').pipe(response);
+	}
 });
 
 server.listen(3000, '127.0.0.1');
