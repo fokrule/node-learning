842c3ed6830a6d7eec275462e9061244f185c780
fokrule <fokrule.hasan"sjinnovation.com>
2019-07-04 06:47:34 +0600

    Worked with Template engine

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 
 var app = express();
 
+app.set('view engine','ejs');
 
  app.get('/', function( req, res ){
 	 res.send('express is found');
@@ -9,6 +10,18 @@ var app = express();
 
  app.get('/profile/:name', function(req, res){
 	res.send('You are viewing profile for ' + req.params.name);
+ });
+ 
+ app.get('/user/:name', function (req, res) {
+	var data = {
+		name : req.params.name,
+		age : 26,
+		Occupation : "Software Engineer",
+		hobbies : [
+			'coading', 'traveling', 'eating'
+		]
+	} 
+	res.render('user', {user: data});	 
  });
 
  app.get('/contact/', function(req, res){
